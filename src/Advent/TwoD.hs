3bedd5ac82a118c170dcc9e7233ba2ac52ee9c51
Dustin Sallings <dustin@spy.net>
2019-12-31 11:18:14 -0800

    Added aroundD (including diagonals)

--- src/Advent/TwoD.hs
+++ src/Advent/TwoD.hs
@@ -5,6 +5,10 @@ type Point = (Int,Int)
 around :: Point -> [Point]
 around (x,y) = [(x,y-1), (x-1,y), (x+1,y), (x,y+1)]
 
+-- Around (including diagonals)
+aroundD :: Point -> [Point]
+aroundD (x,y) = [(x+xo, y+yo) | xo <- [-1..1], yo <- [-1..1], xo /= 0 || yo /= 0]
+
 data Dir = N | E | S | W deriving (Show, Bounded, Enum, Eq)
 
 fwd :: Dir -> (Int,Int) -> (Int,Int)
