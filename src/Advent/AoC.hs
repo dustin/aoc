313753e3738a1371228db4624d452a673347f7af
Dustin Sallings <dustin@spy.net>
2021-12-09 15:48:56 -0800

    Map is no longer used here.

--- src/Advent/AoC.hs
+++ src/Advent/AoC.hs
@@ -28,8 +28,6 @@ module Advent.AoC (
   möb, löb
   ) where
 
-import           Data.Map.Strict       (Map)
-import qualified Data.Map.Strict       as Map
 import           Data.Text             (Text)
 import qualified Data.Text.IO          as TIO
 import           Data.Void             (Void)
